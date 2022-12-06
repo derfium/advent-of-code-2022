913d0aa72eee58da42872ee09539864f91cbfeab
Marcel Mulorz <marcel.mulorz@medicalvalues.de>
2022-12-06 08:57:12 +0100

    Added Day 6 Solutions

---
+++ src/day6.py
@@ -0,0 +1,32 @@
+def task_1():
+    with open("src/input6.txt") as f:
+        line = f.readline().replace("\n", "")
+        
+        for i, _ in enumerate(line, start=4):
+            signal = line[i-4:i]
+            
+            if(checkSignal(signal)):
+                return i
+            
+        return -1
+    
+def task_2():
+    with open("src/input6.txt") as f:
+        line = f.readline().replace("\n", "")
+        
+        startOfPacket = task_1()
+        
+        for i, _ in enumerate(line, start=startOfPacket+14):
+            signal = line[i-14:i]
+            if checkSignal(signal):
+                return i
+    
+    
+def checkSignal(signal:str)->bool:
+    for char in signal:
+        if signal.count(char) != 1:
+            return False
+    return True
+    
+print(task_1())
+print(task_2())
